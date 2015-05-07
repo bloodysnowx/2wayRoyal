fa2762980fb20c84896ce901f881f4136d6ba473
bloodysnow <bloodysnowx@gmail.com>
2015-05-08 00:39:47 +0900

    implement judge till straight

--- 2wayRoyal.sikuli/hand.py
+++ 2wayRoyal.sikuli/hand.py
@@ -2,7 +2,7 @@ suits = ["s", "h", "d", "c"]
 ranks = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"]
 #from itertools import chain
 #aHighRanks = list(chain.from_iterable([[ranks[0]], ranks[9:13]]))
-aHighRanks = [item for sublist in [[ranks[0]], ranks[9:13]] for item in sublist]
+aHighRanks = [item for sublist in [[ranks[0]], ranks[9:]] for item in sublist]
 
 class Hand:
     def __init__(self, cards):
@@ -16,14 +16,23 @@ class Hand:
         aSuit = self.cards[0].suit
         return len(filter(lambda card:card.suit == aSuit, self.cards)) == 5
 
+    def hasEachOfThem(self, ranks):
+        return reduce(lambda x, y: x and y, map(lambda rank: len(filter(lambda card:card.rank == rank, self.cards)) == 1, ranks))
+    
     def isAHighStraight(self):
-        for rank in aHighRanks:
-            if len(filter(lambda card:card.rank == rank, self.cards)) != 1:
-                return False
-        return True
+        return self.hasEachOfThem(aHighRanks)
 
     def isHighRoyal(self):
         return self.isFlush() and self.isAHighStraight()
+
+    def isLowStraight(self):
+        return self.hasEachOfThem(ranks[1:6])
+
+    def isLowRoyal(self):
+        return self.isFlush() and self.isLowStraight()
+
+    def isStraight(self):
+        return reduce(lambda x, y: x or y, map(lambda ranks: self.hasEachOfThem(ranks), map(lambda i: ranks[i:i + 5], range(9)))) or self.isAHighStraight()
 
 class Card:
     def __init__(self, suit, rank, pos):
--- 2wayRoyal.sikuli/hand_test.py
+++ 2wayRoyal.sikuli/hand_test.py
@@ -4,9 +4,62 @@ from hand import Hand
 class HandTest(unittest.TestCase):
     def test_isFlush(self):
         hand = Hand([['s', '01', 0], ['s', '02', 0], ['s', '03', 0], ['s', '04', 0], ['s', '05', 0]])
-        self.assertEqual(True, hand.isFlush())
+        self.assertTrue(hand.isFlush())
         hand = Hand([['c', '01', 0], ['s', '02', 0], ['s', '03', 0], ['s', '04', 0], ['s', '05', 0]])
-        self.assertEqual(False, hand.isFlush())
+        self.assertFalse(hand.isFlush())
+
+    def test_isAHighStraight(self):
+        hand = Hand([['s', '01', 0], ['s', '02', 0], ['s', '03', 0], ['s', '04', 0], ['s', '05', 0]])
+        self.assertFalse(hand.isAHighStraight())
+        hand = Hand([['s', '01', 0], ['c', '12', 0], ['s', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertTrue(hand.isAHighStraight())
+        hand = Hand([['s', '09', 0], ['c', '12', 0], ['s', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertFalse(hand.isAHighStraight())
+
+    def test_isHighRoyal(self):
+        hand = Hand([['s', '01', 0], ['s', '12', 0], ['s', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertTrue(hand.isHighRoyal())
+        hand = Hand([['s', '01', 0], ['s', '12', 0], ['h', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertFalse(hand.isHighRoyal())
+        hand = Hand([['s', '09', 0], ['s', '12', 0], ['s', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertFalse(hand.isHighRoyal())
+
+    def test_isLowStraight(self):
+        hand = Hand([['s', '01', 0], ['s', '12', 0], ['s', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertFalse(hand.isLowStraight())
+        hand = Hand([['s', '09', 0], ['s', '12', 0], ['s', '13', 0], ['s', '11', 0], ['s', '10', 0]])
+        self.assertFalse(hand.isLowStraight())
+        hand = Hand([['s', '02', 0], ['s', '03', 0], ['s', '05', 0], ['s', '06', 0], ['s', '04', 0]])
+        self.assertTrue(hand.isLowStraight())
+        hand = Hand([['s', '02', 0], ['c', '03', 0], ['s', '05', 0], ['h', '06', 0], ['s', '04', 0]])
+        self.assertTrue(hand.isLowStraight())
+        hand = Hand([['s', '02', 0], ['c', '03', 0], ['s', '05', 0], ['h', '01', 0], ['s', '04', 0]])
+        self.assertFalse(hand.isLowStraight())
+
+    def test_isLowRoyal(self):
+        hand = Hand([['s', '02', 0], ['s', '03', 0], ['s', '05', 0], ['s', '06', 0], ['s', '04', 0]])
+        self.assertTrue(hand.isLowRoyal())
+        hand = Hand([['c', '02', 0], ['d', '03', 0], ['s', '05', 0], ['h', '06', 0], ['h', '04', 0]])
+        self.assertFalse(hand.isLowRoyal())
+        hand = Hand([['s', '02', 0], ['s', '03', 0], ['s', '05', 0], ['s', '01', 0], ['s', '04', 0]])
+        self.assertFalse(hand.isLowRoyal())
+
+    def test_isStraight(self):
+        hand = Hand([['s', '02', 0], ['s', '03', 0], ['d', '05', 0], ['s', '01', 0], ['c', '04', 0]])
+        self.assertTrue(hand.isStraight())
+        hand = Hand([['s', '02', 0], ['s', '03', 0], ['d', '13', 0], ['s', '01', 0], ['c', '04', 0]])
+        self.assertFalse(hand.isStraight())
+        hand = Hand([['s', '12', 0], ['s', '13', 0], ['d', '11', 0], ['s', '01', 0], ['c', '10', 0]])
+        self.assertTrue(hand.isStraight())
+        hand = Hand([['s', '07', 0], ['s', '08', 0], ['d', '11', 0], ['s', '09', 0], ['c', '10', 0]])
+        self.assertTrue(hand.isStraight())
+        hand = Hand([['s', '05', 0], ['s', '08', 0], ['d', '11', 0], ['s', '09', 0], ['c', '10', 0]])
+        self.assertFalse(hand.isStraight())
+        hand = Hand([['s', '11', 0], ['s', '08', 0], ['d', '11', 0], ['s', '09', 0], ['c', '10', 0]])
+        self.assertFalse(hand.isStraight())
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
